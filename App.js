79c4c8750ad8848b93e69d68637689f00ab14b7b
sgarcia <sgarcia@truora.com>
2022-07-18 13:24:41 -0400

    testaxios

--- App.js
+++ App.js
@@ -2,17 +2,18 @@
 import { View, Text } from 'react-native';
 import { useState, useEffect } from 'react';
 import Cedula from './Cedula';
+import axios from 'axios';
 
 export default function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch(
+      const result = await axios.get(
         'http://cedulaapifinal.somee.com/api/Cedulas',
       );
 
-      const json = await result.json()
+      const json = result.data
 
       setData(json);
     };
